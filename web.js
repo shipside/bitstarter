b2666b4b72c6783ee1780c746544a0e134e502a5
zhchen <shipside@gmail.com>
2013-07-10 00:00:41 +0800

    read file success

--- web.js
+++ web.js
@@ -2,11 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var infile = "index.html";
+var buf=fs.readFileSync(infile); 
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  //response.send('Hello World!');
+  response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
